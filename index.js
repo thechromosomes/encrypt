093c827b351522752cfe58040bbaee88a655f8ef
chromosome <48309910+thechromosomes@users.noreply.github.com>
2023-07-20 12:35:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,6 +12,106 @@ app.use(bodyParser.json());
 let privateKey = process.env.PRIVATE_KEY_PATH;
 let publicKey = process.env.PUBLIC_KEY_PATH;
 
+let DB_MOCKUP = [
+  {
+    ocpVersion: "2.0.1",
+    action: "charge",
+    requestId: "123456789",
+    vinHash: "9a73bfc3ddb1e353f7fb2fcbf055255f347d206057e3601343337f006555afbd",
+    vinActual: "VID:ABCdef12",
+    chargingParameters: {
+      maxPower: 7.4,
+      chargingType: "AC",
+      connectorType: "Type 2",
+      startTime: "2023-07-20T10:00:00",
+      endTime: "2023-07-20T14:00:00",
+      chargingRateLimit: 5.0,
+      maxCapacity: 50.0,
+      chargingProfile: "normal",
+      isReservation: true,
+    },
+  },
+  {
+    ocpVersion: "2.0.1",
+    action: "charge",
+    requestId: "987654321",
+    vinHash: "c15e0cbeff8d0e1ad6858f03817b936e8a0fb7e9ad18357146f71ab29a19e364",
+    vinActual: "VID:xyzGHI89",
+    chargingParameters: {
+      maxPower: 11.0,
+      chargingType: "DC",
+      connectorType: "CCS Combo",
+      startTime: "2023-07-20T12:00:00",
+      endTime: "2023-07-20T18:00:00",
+      chargingRateLimit: 10.0,
+      maxCapacity: 70.0,
+      chargingProfile: "fast",
+      isReservation: false,
+    },
+  },
+  {
+    ocpVersion: "2.0.1",
+    action: "charge",
+    requestId: "246813579",
+    vinHash: "45e4662fc303e26f76d06e6960db3c69e26ec1e2dc9f3ab68a29300fc42c9bec",
+    vinActual: "VID:pqrSTU34",
+    chargingParameters: {
+      maxPower: 3.6,
+      chargingType: "AC",
+      connectorType: "Type 1",
+      startTime: "2023-07-20T09:00:00",
+      endTime: "2023-07-20T15:00:00",
+      chargingRateLimit: 2.0,
+      maxCapacity: 40.0,
+      chargingProfile: "slow",
+      isReservation: true,
+    },
+  },
+  {
+    ocpVersion: "2.0.1",
+    action: "charge",
+    requestId: "135792468",
+    vinHash: "c1851fe4dcb9c451017bf22a2d5604b2786f8105350e89f479d5ecd33e384ffb",
+    vinActual: "VID:lmnOPQ56",
+    chargingParameters: {
+      maxPower: 22.0,
+      chargingType: "DC",
+      connectorType: "Tesla Supercharger",
+      startTime: "2023-07-20T08:00:00",
+      endTime: "2023-07-20T13:00:00",
+      chargingRateLimit: 20.0,
+      maxCapacity: 100.0,
+      chargingProfile: "supercharge",
+      isReservation: false,
+    },
+  },
+  {
+    ocpVersion: "2.0.1",
+    action: "charge",
+    requestId: "314159265",
+    vinHash: "d8b5e5d775a21473788740452425b12e0ace1b771a93019e28f651712254cf6a",
+    vinActual: "VID:uvwXYZ78",
+    chargingParameters: {
+      maxPower: 7.4,
+      chargingType: "AC",
+      connectorType: "Type 2",
+      startTime: "2023-07-20T10:30:00",
+      endTime: "2023-07-20T17:00:00",
+      chargingRateLimit: 6.0,
+      maxCapacity: 55.0,
+      chargingProfile: "normal",
+      isReservation: true,
+    },
+  },
+];
+
+function encryptData(data) {
+  const hash = crypto.createHash("sha256");
+  hash.update(JSON.stringify(data));
+  const hashedData = hash.digest("hex");
+  return hashedData;
+}
+
 app.post("/encrypt", (req, res) => {
   try {
     function encryptData(data) {
@@ -22,7 +122,6 @@ app.post("/encrypt", (req, res) => {
     const encryptedData = encryptData(data);
     res.send({ encryptedData });
   } catch (error) {
-    console.log(error)
     res.send("Error occurred", error);
   }
 });
@@ -40,25 +139,13 @@ app.post("/decrypt", (req, res) => {
     const decryptedData = decryptData(encryptedData);
     res.send(decryptedData);
   } catch (error) {
-    console.log(error)
     res.send("Error occurred");
   }
 });
 
 app.post("/on-way-encrypt", (req, res) => {
   try {
-    function encryptData(data) {
-      const hash = crypto.createHash("sha256");
-      hash.update(JSON.stringify(data));
-      const hashedData = hash.digest("hex");
-
-      const encryptedData = crypto
-        .publicEncrypt(publicKey, Buffer.from(hashedData, "hex"))
-        .toString("hex");
-      return encryptedData;
-    }
-
-    const data = req.body.vehicleData;
+    const data = req.body.vehicleId;
     const encryptedData = encryptData(data);
     res.send({ encryptedData });
   } catch (error) {
@@ -68,30 +155,21 @@ app.post("/on-way-encrypt", (req, res) => {
 
 app.post("/verify-on-way-encrypt", (req, res) => {
   try {
-    function verifyData(data, encryptedData) {
-      try {
-        const decryptedData = crypto
-          .privateDecrypt(privateKey, Buffer.from(encryptedData, "hex"))
-          .toString("hex");
+    const vehicleId = req.body.vehicleId;
+    let finalData = DB_MOCKUP.filter((elem) => elem.vinHash === vehicleId);
+    let result;
 
-        const computedHash = crypto
-          .createHash("sha256")
-          .update(JSON.stringify(data))
-          .digest("hex");
-
-        return computedHash === decryptedData;
-      } catch (error) {
-        return false; 
-      }
+    if (finalData.length) {
+      result = finalData[0].chargingParameters;
+    } else {
+      throw "Invalid ID";
     }
 
-    const data = req.body.vehicleData;
-    const encryptedData = req.body.encryptedData;
-
-    const verified = verifyData(data, encryptedData);
-    res.send({ verified });
+    res.status(200).send({
+      result,
+    });
   } catch (error) {
-    res.send("Error occurred");
+    res.status(500).send("Error occurred");
   }
 });
 
